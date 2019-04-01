298df4fb9bc661487264fdd7f7b5484e9a010cb9
Ian Christner and Zema Kebede <zema@>
2019-04-01 10:39:03 -0700

    Add forEach loop to create array

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,23 @@
 $(document).ready(function(){
-  var answerArr = [3];
+  var answerArr = [];
   $('#myForm').submit(function(event){
     event.preventDefault();
     //console.log("myForm is being submitted");
-    var favAnimal =  $('#inputAnimal').val();
+    //var favAnimal =  $('#inputAnimal').val();
     //console.log(favAnimal);
-    var favFood = $('#inputFood').val();
+    //var favFood = $('#inputFood').val();
     //console.log(favFood);
-    var favPlace = $('#inputPlace').val();
+    //var favPlace = $('#inputPlace').val();
     //console.log(favPlace);
-    answerArr = [favAnimal, favFood, favPlace];
+    //answerArr = [favAnimal, favFood, favPlace];
     //  console.log("Your answer Array looks like [" + answerArr + "]!");
+    var input = ['Animal', 'Food', 'Place'];
+    input.forEach(function(element){
+      var myInput = $('#input' + element).val();
+      answerArr.push(myInput);
+      console.log('The element, '+ element + ' will make our array look like this ['+ answerArr + ']!');
+    });
+
     newArr = [];
     newArr.push(answerArr[1]);
     newArr.push(answerArr[0]);
@@ -23,7 +30,5 @@ $(document).ready(function(){
     $('#secondList').text(newArr[1]);
     $("#answerList").append('<li id="thirdList"></li>');
     $('#thirdList').text(newArr[2]);
-
-
   });
 });
